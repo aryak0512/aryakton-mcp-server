4a7078fa99accec46834f7bedc950fdd09a705d9
Aryak Deshpande <aryak.deshpande0512@gmail.com>
2025-12-25 22:55:51 +0530

    Tools added

--- src/main/java/com/aryak/mcp/AryaktonMcpServerApplication.java
+++ src/main/java/com/aryak/mcp/AryaktonMcpServerApplication.java
@@ -2,11 +2,13 @@ package com.aryak.mcp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class AryaktonMcpServerApplication {
 
-    public static void main(String[] args) {
+    static void main(String[] args) {
         SpringApplication.run(AryaktonMcpServerApplication.class, args);
     }
 
---
+++ src/main/java/com/aryak/mcp/tools/MyTools.java
@@ -0,0 +1,39 @@
+package com.aryak.mcp.tools;
+
+import org.springaicommunity.mcp.annotation.McpTool;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.concurrent.atomic.AtomicLong;
+
+@Component
+public class MyTools {
+
+    private final AtomicLong counter = new AtomicLong(1_00_000);
+
+    /**
+     * @return the ZonedDateTime in a fictional town called Aryakton
+     */
+    @McpTool(name = "Get LocalTime In Aryakton",
+            description = "returns the current local time in a fictional town called 'Aryakton'")
+    public LocalDateTime getTimeInAryakton() {
+        return LocalDateTime.now(ZoneId.of("Europe/London"));
+    }
+
+    /**
+     * @return the population in a fictional town called Aryakton
+     */
+    @McpTool(name = "Get current population In Aryakton",
+            description = "returns the current population of the fictional town called 'Aryakton'")
+    public long getCurrentPopulationInAryakton() {
+        return counter.get();
+    }
+
+    // for simulation
+    @Scheduled(fixedDelay = 5)
+    void incrementCounter() {
+        counter.incrementAndGet();
+    }
+}
