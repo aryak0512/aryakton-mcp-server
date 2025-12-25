3f09ef4cc8b179a4788a10f8b378ee6cfd47271d
Aryak Deshpande <aryak.deshpande0512@gmail.com>
2025-12-25 22:27:36 +0530

    Initial project structure

---
+++ src/main/java/com/aryak/mcp/AryaktonMcpServerApplication.java
@@ -0,0 +1,13 @@
+package com.aryak.mcp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AryaktonMcpServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AryaktonMcpServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aryak/mcp/AryaktonMcpServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aryak.mcp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AryaktonMcpServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
